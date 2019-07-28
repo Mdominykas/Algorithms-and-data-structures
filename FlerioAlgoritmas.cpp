b895a8fdcb4bfc28d1a58d83b1e17bd537843326
Mdominykas <marmadominykas@gmail.com>
2019-07-28 18:01:00 +0300

    Hamiltono ir Oilerio keliai

---
+++ FlerioAlgoritmas.cpp
@@ -0,0 +1,117 @@
+/*input
+5 4
+1 2
+2 3
+3 4
+4 1
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+const int max_n = 10000;
+int briaunos[max_n][max_n];
+int briaunu_skaicius[max_n];
+int tiltai[max_n][max_n];
+vector<int> kelias;
+int aplankyta[max_n];
+
+void paieska_platyn(int x)
+{
+	aplankyta[x] = 1;
+	for(int i=0; i<max_n; i++)
+	{
+		if((briaunos[x][i]>0) && (aplankyta[i]==0))
+			paieska_platyn(i);
+	}
+}
+
+int tiltas(int x, int y)
+{
+	if(briaunos[x][y]>1)
+		return false;
+	else if(tiltai[x][y]==1)
+		return true;
+	else
+	{
+		briaunos[x][y]--;
+		briaunos[y][x]--;
+		for(int i=0; i<max_n; i++)
+			aplankyta[i] = 0;
+		paieska_platyn(x);
+		briaunos[x][y]++;
+		briaunos[y][x]++;
+		tiltai[x][y] = 1-aplankyta[y];
+		return tiltai[x][y];
+	}
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int nelyg = 0;
+	int virsune = 0;
+	for(int i=0; i<n; i++)
+	{
+		briaunu_skaicius[i] = 0;
+		for(int j=0; j<n; j++)
+		{
+			briaunos[i][j] = 0;
+			tiltai[i][j] = 0;
+		}
+	}
+	for(int i=0; i<m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		briaunos[a][b]++;
+		briaunos[b][a]++;
+		briaunu_skaicius[a]++;
+		briaunu_skaicius[b]++;
+		virsune = a;
+	}
+	for(int i=0; i<n; i++)
+	{
+		if(briaunu_skaicius[i]%2!=0)
+		{
+			nelyg++;
+			virsune = i;
+		}
+	}
+	int dalykas = 0 ;
+	kelias.push_back(virsune);
+	if((nelyg==0) || (nelyg==2))
+	{
+		while(briaunu_skaicius[virsune]!=0)
+		{
+			dalykas++;
+			int suma = 0;
+			for(int i=0; i<n; i++)
+			{
+				suma += briaunos[virsune][i];
+				if(briaunos[i][virsune]>0)
+				{
+					if((tiltas(virsune, i)==0) || (suma==briaunu_skaicius[virsune]))
+					{
+						kelias.push_back(i);
+						briaunos[virsune][i]--;
+						briaunos[i][virsune]--;
+						briaunu_skaicius[virsune]--;
+						briaunu_skaicius[i]--;
+						virsune = i;
+						break;
+					}
+				}
+			}
+		}
+	}
+	if(kelias.size()==m+1)
+	{
+		cout << "kelias: "; 
+		for(int i=0; i<kelias.size(); i++)
+			cout << kelias[i] << " ";
+	}
+	else
+		cout << "kelias neegzistuoja";
+	return 0;
+}
---
+++ HamiltonoKeliams.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int max_n = 100;
+vector<int> kaimynai[max_n];
+int seka[max_n];
+bool aplankyta[max_n];
+
+bool ieskok(int dabartine, int liko)
+{
+	aplankyta[dabartine] = true;
+	seka[liko] = dabartine;
+	if(liko==0)
+	{
+		aplankyta[dabartine] = false;
+		return true;
+	}
+	for(int i=0; i<kaimynai[dabartine].size(); i++)
+	{
+		if(aplankyta[kaimynai[dabartine][i]]==false)
+		{
+			if(ieskok(kaimynai[dabartine][i], liko-1)==true)
+			{
+				aplankyta[dabartine] = false;
+				return true;
+			}
+
+		}
+	}
+	aplankyta[dabartine] = false;
+	return false;
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	for(int i=0; i<m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		kaimynai[a].push_back(b);
+		kaimynai[b].push_back(a);
+	}
+	for(int i=0; i<n; i++)
+	{
+		aplankyta[i] = false;
+		seka[i] = -1;
+	}
+	for(int i=0; i<n; i++)
+	{
+		if(ieskok(i, n-1)==true)
+		{
+			for(int j=n-1; j>=0; j--)
+				cout << seka[j] << " ";
+			cout << endl;
+		}
+	}
+	return 0;
+}
