a6b98b2f6d58d4eaadc87269157bc5651cb74cfa
Mdominykas <marmadominykas@gmail.com>
2021-09-28 13:19:36 +0300

    fixed formatting

--- Data Structures/SparseTable.cpp
+++ Data Structures/SparseTable.cpp
@@ -15,22 +15,22 @@ int lookup[max_N][logn];
 int loga[max_N];
 void buildsparsetable(int N, int a[])
 {
-	for(int i=0; i<N; i++)
+	for(int i = 0; i < N; i++)
 		lookup[i][0] = a[i];
-	for(int j=1; j<logn; j++)
+	for(int j = 1; j < logn; j++)
 	{
-		for(int i=0; i+(1<<j)<=N; i++)
-			lookup[i][j] = min(lookup[i][j-1], lookup[i+(1<<(j-1))][j-1]);
+		for(int i = 0; i + (1<<j) <= N; i++)
+			lookup[i][j] = min(lookup[i][j - 1], lookup[i + (1<<(j - 1))][j - 1]);
 	}
 	loga[1] = 0;
-	for(int i=2; i<=N; i++)
-		loga[i] = loga[i/2] + 1;
+	for(int i = 2; i <= N; i++)
+		loga[i] = loga[i / 2] + 1;
 }
 
 int query(int L, int R)
 {
-	int sk = loga[R-L+1];
-	return min(lookup[L][sk], lookup[R-(1<<sk)+1][sk]);
+	int sk = loga[R - L + 1];
+	return min(lookup[L][sk], lookup[R - (1<<sk) + 1][sk]);
 }
 
 int main()
